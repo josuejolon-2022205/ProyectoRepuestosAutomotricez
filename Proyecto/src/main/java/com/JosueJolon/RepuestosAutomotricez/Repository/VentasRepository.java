ba6c0baea99183cff21a9f675d602f4bb53b797a
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-13 19:22:20 -0600

    refactor: se ha modificado el repository de ventas

---
+++ Proyecto/src/main/java/com/JosueJolon/RepuestosAutomotricez/Repository/VentasRepository.java
@@ -0,0 +1,9 @@
+package com.JosueJolon.RepuestosAutomotricez.Repository;
+
+import com.JosueJolon.RepuestosAutomotricez.Entity.Ventas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentasRepository extends JpaRepository<Ventas, Integer> {
+}
