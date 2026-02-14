45c7979e281ff7e66169deb801e4b0865cbdd0b2
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-02-13 19:24:30 -0600

    feat: se ha agregado la clase exception de ResouceNotFoundException

---
+++ Proyecto/src/main/java/com/JosueJolon/RepuestosAutomotricez/Exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.JosueJolon.RepuestosAutomotricez.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
